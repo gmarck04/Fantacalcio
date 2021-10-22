b361a15b18d6686a0deb37e9c1ad525eb96e44db
Giovanni <gmarck04@gmail.com>
2021-10-22 09:27:17 +0200

    Modifica codice

--- Fantacalcio/Fantacalcio/Program.cs
+++ Fantacalcio/Fantacalcio/Program.cs
@@ -17,19 +17,39 @@
     Il progetto DEVE essere svolto in modalità CONSOLE. */
 
 using System;
+using System.Collections.Generic;
 
 namespace Fantacalcio
 {
-    class fantacalcio
+    class fantacalciatore
+    {
+        //Attributi
+        string nome, ruolo;
+        int prezzo, id;
+        //Costruttore
+        public fantacalciatore(string nome, string ruolo, int prezzo, int id)
+        {
+            this.nome = nome;
+            this.ruolo = ruolo;
+            this.prezzo = prezzo;
+            this.id = id;
+        }
+        public override string ToString()
+        {
+            return $"il nome {nome}, il ruolo {ruolo}, il prezzo {prezzo}, l'id {id}";
+        }
+    }
+    class fantaallenatore
     {
         //Attributi
         protected string[] Fantaallenatore = new string[0];
         protected int[] Fantaallenatore_crediti = new int[0];
         protected int[] Fantaallenatore_punti = new int[0];
         public int giorni_totali { get; set; }
+        public List<fantacalciatore> Lista_Calciatori = new List<fantacalciatore>();
 
         //Costruttore
-        public fantacalcio()
+        public fantaallenatore() //inserisci la data
         {
             this.giorni_totali = 0;
         }
@@ -49,6 +69,10 @@ namespace Fantacalcio
         {
 
         }
+        public void Aggiungi_Fantagiocatore(fantacalciatore calciatore)
+        {
+            Lista_Calciatori.Add(calciatore);
+        }
         public void stampa()
         {
             for(int i = 0; i< Fantaallenatore.Length; i++)
@@ -56,15 +80,21 @@ namespace Fantacalcio
                 Console.WriteLine(Fantaallenatore[i]);
                 Console.WriteLine(Fantaallenatore_crediti[i]);
                 Console.WriteLine(Fantaallenatore_punti[i]);
-            }      
+            }
+            foreach (fantacalciatore calciatore in Lista_Calciatori)
+            {
+                Console.WriteLine(calciatore.ToString());
+            }          
         }
     }
     class Program
     {
         static void Main(string[] args)
         {
-            fantacalcio c = new fantacalcio();
+            fantaallenatore c = new fantaallenatore();
             int valore = 1;
+            int valore_calciatore = 1;
+            int id = 0;
             
             Console.WriteLine("Inserisci il credito del fantaallenatore");
             int crediti = Int32.Parse(Console.ReadLine());
@@ -74,6 +104,26 @@ namespace Fantacalcio
                 c.Inserisci_nome_Fantaallenatore(Console.ReadLine());
                 c.Inserisci_crediti_Fantaallenatore(crediti);
 
+                do
+                {
+                    Console.WriteLine("nome giocatore");
+                    string nome = Console.ReadLine();
+                    Console.WriteLine("ruolo giocatore");
+                    string ruolo = Console.ReadLine();
+                    Console.WriteLine("prezzo giocatore");
+                    int prezzo = Int32.Parse(Console.ReadLine());
+
+                    c.Aggiungi_Fantagiocatore(new fantacalciatore(nome, ruolo, prezzo, id));
+
+                    Console.WriteLine("Vuoi continuare? Inserisci 1 per stoppare e 0 per continuare");
+                    valore_calciatore = Int32.Parse(Console.ReadLine());
+                    while (valore_calciatore != 0 && valore_calciatore != 1)
+                    {
+                        Console.WriteLine("Errato: se vuoi continuare? Inserisci 1 per stoppare e 0 per continuare");
+                        valore_calciatore = Int32.Parse(Console.ReadLine());
+                    }
+                } while (valore_calciatore == 0);
+
                 Console.WriteLine("Vuoi continuare? Inserisci 1 per stoppare e 0 per continuare");
                 valore = Int32.Parse(Console.ReadLine());
                 while (valore != 0 && valore != 1)
@@ -81,9 +131,8 @@ namespace Fantacalcio
                     Console.WriteLine("Errato: se vuoi continuare? Inserisci 1 per stoppare e 0 per continuare");
                     valore = Int32.Parse(Console.ReadLine());
                 }
+                id++;
             } while (valore == 0);
-
-            
 
             c.stampa();
         }
